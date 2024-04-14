9159d2ca2d23adc0fe2f9735bafdc3162ebddd58
Markus Gasser <markus.gasser@mailbox.org>
2024-04-14 08:41:51 +0200

    Update config

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,3 +1,3 @@
-function dotfiles --wraps='git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME' --description 'alias dotfiles=git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
-  git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
+function dotfiles --wraps='git --git-dir=$HOME/.local/state/dotfiles/repository.git/ --work-tree=$HOME' --description 'alias dotfiles=git --git-dir=$HOME/.local/state/dotfiles/repository.git/ --work-tree=$HOME'
+  git --git-dir=$HOME/.local/state/dotfiles/repository.git/ --work-tree=$HOME $argv
 end
